ff9120e6aefc152efa68a30c64e25ca8502b337d
alimaijidi62 <a.m.majidi.62@gmail.com>
2025-10-07 22:05:22 +0200

    revert back to the old one

--- testcpp20/FoodRating.cpp
+++ testcpp20/FoodRating.cpp
@@ -1,22 +1,14 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
-#include <queue>
+#include <map>
+#include <set>
 using namespace std;
-
-struct FoodEntry {
-    int rating;
-    string name;
-    bool operator<(const FoodEntry& other) const {
-        if (rating != other.rating) return rating < other.rating; // max-heap by rating
-        return name > other.name; // min-heap by name for ties
-    }
-};
 
 class FoodRatings {
     unordered_map<string, string> foodToCuisine;
     unordered_map<string, int> foodToRating;
-    unordered_map<string, priority_queue<FoodEntry>> cuisineHeap;
+    unordered_map<string, map<int, set<string>>> cuisineRatings;
 
 public:
     FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
@@ -24,22 +16,25 @@ public:
         for (int i = 0; i < n; ++i) {
             foodToCuisine[foods[i]] = cuisines[i];
             foodToRating[foods[i]] = ratings[i];
-            cuisineHeap[cuisines[i]].push({ ratings[i], foods[i] });
+            cuisineRatings[cuisines[i]][ratings[i]].insert(foods[i]);
         }
     }
-
+    
     void changeRating(string food, int newRating) {
         string cuisine = foodToCuisine[food];
+        int oldRating = foodToRating[food];
+        // Remove from old rating set
+        auto& ratingSet = cuisineRatings[cuisine][oldRating];
+        ratingSet.erase(food);
+        if (ratingSet.empty()) cuisineRatings[cuisine].erase(oldRating);
+        // Add to new rating set
+        cuisineRatings[cuisine][newRating].insert(food);
         foodToRating[food] = newRating;
-        cuisineHeap[cuisine].push({ newRating, food });
     }
-
+    
     string highestRated(string cuisine) {
-        auto& heap = cuisineHeap[cuisine];
-        while (true) {
-            FoodEntry top = heap.top();
-            if (foodToRating[top.name] == top.rating) return top.name;
-            heap.pop(); // Remove outdated entry
-        }
+        auto& ratingsMap = cuisineRatings[cuisine];
+        auto it = ratingsMap.rbegin(); // highest rating
+        return *(it->second.begin()); // lex smallest food name
     }
 };
