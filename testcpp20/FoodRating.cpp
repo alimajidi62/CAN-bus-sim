fd773bcb86039726d5be5cf9116d59cc868e970f
alimaijidi62 <a.m.majidi.62@gmail.com>
2025-10-21 11:34:49 +0200

    food rating is now comment out

--- testcpp20/FoodRating.cpp
+++ testcpp20/FoodRating.cpp
@@ -1,39 +1,39 @@
-#include <string>
-#include <vector>
-#include <unordered_map>
-#include <map>
-#include <set>
-using namespace std;
-
-class FoodRatings {
-    unordered_map<string, string> foodToCuisine;
-    unordered_map<string, int> foodToRating;
-    unordered_map<string, map<int, set<string>>> cuisineRatings;
-
-public:
-    FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
-        int n = foods.size();
-        for (int i = 0; i < n; ++i) {
-            foodToCuisine[foods[i]] = cuisines[i];
-            foodToRating[foods[i]] = ratings[i];
-            cuisineRatings[cuisines[i]][ratings[i]].insert(foods[i]);
-        }
-    }
-    
-    void changeRating(string food, int newRating) {
-        string cuisine = foodToCuisine[food];
-        int oldRating = foodToRating[food];
-        // Remove from old rating set
-        auto& ratingSet = cuisineRatings[cuisine][oldRating];
-        ratingSet.erase(food);
-        if (ratingSet.empty()) cuisineRatings[cuisine].erase(oldRating);
-        // Add to new rating set
-        cuisineRatings[cuisine][newRating].insert(food);
-        foodToRating[food] = newRating;
-    }
-    
-    string highestRated(string cuisine) {
-        auto& ratingsMap = cuisineRatings[cuisine];
-        auto it = ratingsMap.rbegin(); 
-        return *(it->second.begin()); 
-};
+//#include <string>
+//#include <vector>
+//#include <unordered_map>
+//#include <map>
+//#include <set>
+//using namespace std;
+//
+//class FoodRatings {
+//    unordered_map<string, string> foodToCuisine;
+//    unordered_map<string, int> foodToRating;
+//    unordered_map<string, map<int, set<string>>> cuisineRatings;
+//
+//public:
+//    FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
+//        int n = foods.size();
+//        for (int i = 0; i < n; ++i) {
+//            foodToCuisine[foods[i]] = cuisines[i];
+//            foodToRating[foods[i]] = ratings[i];
+//            cuisineRatings[cuisines[i]][ratings[i]].insert(foods[i]);
+//        }
+//    }
+//    
+//    void changeRating(string food, int newRating) {
+//        string cuisine = foodToCuisine[food];
+//        int oldRating = foodToRating[food];
+//        // Remove from old rating set
+//        auto& ratingSet = cuisineRatings[cuisine][oldRating];
+//        ratingSet.erase(food);
+//        if (ratingSet.empty()) cuisineRatings[cuisine].erase(oldRating);
+//        // Add to new rating set
+//        cuisineRatings[cuisine][newRating].insert(food);
+//        foodToRating[food] = newRating;
+//    }
+//    
+//    string highestRated(string cuisine) {
+//        auto& ratingsMap = cuisineRatings[cuisine];
+//        auto it = ratingsMap.rbegin(); 
+//        return *(it->second.begin()); 
+//};
